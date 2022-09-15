271de1793b06c083962cacd252175fe518527eba
pattama <pat_kadae@hotmail.com>
2022-09-15 14:20:36 +0200

    add Tcells Fibro h5ad to data folder

--- pages/Tcells_Fibroblast.py
+++ pages/Tcells_Fibroblast.py
@@ -5,8 +5,17 @@ import anndata as ad
 import scanpy as sc
 import squidpy as sq
 
+from utils.utils import *
 
 st.markdown("""
 # Tcells & Fibroblast subcell classes
 """
             )
+
+# 1. import anndata
+
+img_name = st.sidebar.selectbox(
+    "Select image", ('R2(Basal like)', 'R4(ER+)', 'R5(HER2+)'))
+img = img_name.split('(')[0]
+
+adata = sc.read_h5ad('Tcells/Epi_Fibro_'+img+'_spatial.h5ad')
