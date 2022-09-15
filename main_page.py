ff864359d8c40e882c042843f752f5b39d6e0254
pattama <pat_kadae@hotmail.com>
2022-09-15 13:35:03 +0200

    add main_page.py

---
+++ main_page.py
@@ -0,0 +1,14 @@
+from matplotlib import pyplot as plt
+import streamlit as st
+import anndata as ad
+import scanpy as sc
+import squidpy as sq
+
+
+st.markdown("""
+# Spatial plot app
+## This web-apps is to check position of subcell class between selected cell classes
+"""
+            )
+
+
