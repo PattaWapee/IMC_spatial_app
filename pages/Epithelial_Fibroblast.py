4c884d521658fbf86b8e6ec39a5e3233c117769f
pattama <pat_kadae@hotmail.com>
2022-09-16 12:48:25 +0200

    error

--- pages/Epithelial_Fibroblast.py
+++ pages/Epithelial_Fibroblast.py
@@ -3,12 +3,13 @@ import streamlit as st
 import anndata as ad
 import scanpy as sc
 import squidpy as sq
+import pandas as pd
 
 
 st.markdown("""
-# Epithelial & Fibroblast subcell classes
+    # Epithelial & Fibroblast subcell classes
 """
-            )
+        )
 
 st.sidebar.markdown("# Epithelial vs Fibroblast")
 
@@ -19,31 +20,31 @@ def plt_spatial(adata, obs_col):
     color is labeled from chosen obs column
     '''
     fig = sc.pl.spatial(adata, color=obs_col,
-                        title=[obs_col], spot_size=10,
-                        show=False, return_fig=True)
+                    title=[obs_col], spot_size=10,
+                    show=False, return_fig=True)
     fig = fig[0].get_figure()
-    return fig
+return fig
 
 
 def filter_adata(adata, obs_col, obs_value_ls):
-    '''
-    Filtering anndata by list of obs value
-    '''
+'''
+Filtering anndata by list of obs value
+'''
     adata_filtered = adata[adata.obs[obs_col].isin(obs_value_ls)]
-    return adata_filtered
+return adata_filtered
 
 # 1. import anndata
 
 
 img_name = st.sidebar.selectbox(
-    "Select image", ('R2(Basal like)', 'R4(ER+)', 'R5(HER2+)'))
+"Select image", ('R2(Basal like)', 'R4(ER+)', 'R5(HER2+)'))
 img = img_name.split('(')[0]
 
 adata = sc.read_h5ad('data/Epi_Fibro_'+img+'_spatial.h5ad')
 
 # 2. choose marker and show spatial plot for all cluster
 epi_marker = st.sidebar.selectbox(
-    "Select epithelial marker label", adata.obs.columns)
+"Select epithelial marker label", adata.obs.columns)
 fig = plt_spatial(adata, epi_marker)
 st.write('## Spatial plot for ' + img)
 st.pyplot(fig)
@@ -68,9 +69,9 @@ color = col3.text_input('red')
 
 run = st.button('Submit')
 new_input_df = pd.DataFrame({'Cluster_list':cl_input2,
-                        'name':name_merged,
-                        'color':color
-                        })
+                    'name':name_merged,
+                    'color':color
+                    })
 
 if run:
     st.session_state.userdf = pd.concat([st.session_state.userdf, new_input_df], axis = 0)
