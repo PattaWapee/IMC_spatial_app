3e7300950967a4305c2000d49003c462691de1ac
pattama <pat_kadae@hotmail.com>
2022-09-16 23:48:49 +0200

    add features of merging cluster and choose color for epithelial vs Fibroblast

--- pages/Epithelial_Fibroblast.py
+++ pages/Epithelial_Fibroblast.py
@@ -64,7 +64,26 @@ st.write('### Plotting merged clusters')
 
 n_clusters = st.sidebar.text_input('How many clusters for new plot',2)
 
+adata_merge_ls = []
+color_ls = []
+name_ls = []
 for i in range(int(n_clusters)):
-    cl = st.sidebar.text_input('merged list of cluster '+str(i+1), '2,4' )
-    name = st.sidebar.text_input('name of merged cluster '+str(i+1), 'FibroA' )
-    st.sidebar.color_picker('color merged cluster '+str(i+1), '#00f900')
+    cl = st.sidebar.text_input('merged list of cluster '+str(i+1), str(i+3)+','+str(i+4) )
+    name = st.sidebar.text_input('name of merged cluster '+str(i+1), 'Cluster'+str(i+1) )
+    color = st.sidebar.color_picker('color merged cluster '+str(i+1), '#ff34ff')
+    cl_merge_ls = cl.split(',')
+    adata_merge = filter_adata(adata, epi_marker, cl_merge_ls)
+    adata_merge.obs['merged'] = name
+    adata_merge_ls.append(adata_merge)
+    color_ls.append(color)
+    name_ls.append(name)
+
+adata_mergedall = sc.concat(adata_merge_ls)
+adata_mergedall.obs['merged'] = adata_mergedall.obs['merged'].astype('category')
+adata_mergedall.obs['merged'] = adata_mergedall.obs['merged'].cat.reorder_categories(name_ls)
+adata_mergedall.uns['merged_colors'] = color_ls
+fig3 = plt_spatial(adata_mergedall, 'merged')
+st.pyplot(fig3)
+
+    
+
